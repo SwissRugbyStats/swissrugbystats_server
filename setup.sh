5e282a6f1c13dadb18dbf0ec6a8b8b43d443f178
platzhersh <chregi.glatthard@gmail.com>
2015-01-12 22:47:03 +0100

    add migrate to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 ./setup/get-pip.py
 pip install virtualenv
 virtualenv --no-site-packages --distribute ./env && source ./env/bin/activate && pip install -r requirements.txt
+python manage.py migrate
