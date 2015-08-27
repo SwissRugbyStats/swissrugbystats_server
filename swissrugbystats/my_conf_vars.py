22c873e3dadd3a7c3cc19660b8f54a2859d99308
platzhersh <chregi.glatthard@gmail.com>
2015-08-27 03:28:07 +0200

    remove functions from conf vars file

--- swissrugbystats/my_conf_vars.py
+++ swissrugbystats/my_conf_vars.py
@@ -8,27 +8,3 @@ config.set("ARCHIVE_BASE_URL", default="http://www.suisserugby.com/competitions/
 config.set("FIXTURES_URL_ENDING", default="/lt/fixtures.html")
 config.set("RESULTS_URL_ENDING", default="/lt/results.html")
 config.set("LEAGUE_URL_ENDING", default=".html")
-
-
-def get_league_url(league):
-    return "{}{}{}".format(config.COMPETITIONS_BASE_URL, league, config.LEAGUE_URL_ENDING)
-
-
-def get_fixtures_url(league):
-    return "{}{}{}".format(config.COMPETITIONS_BASE_URL, league, config.FIXTURES_URL_ENDING)
-
-
-def get_results_url(league):
-    return "{}{}{}".format(config.COMPETITIONS_BASE_URL, league, config.RESULTS_URL_ENDING)
-
-
-def get_archive_league_url(league, season_slug):
-    return "{}{}/{}{}".format(config.ARCHIVE_BASE_URL, season_slug, league, config.LEAGUE_URL_ENDING)
-
-
-def get_archive_fixtures_url(league, season_slug):
-    return "{}{}/{}{}".format(config.ARCHIVE_BASE_URL, season_slug, league, config.FIXTURES_URL_ENDING)
-
-
-def get_archive_results_url(league, season_slug):
-    return "{}{}/{}{}".format(config.ARCHIVE_BASE_URL, season_slug, league, config.RESULTS_URL_ENDING)
