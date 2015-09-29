fb1d1eb4dc521a8e8ff68db3ab1b6bfaa3e54992
platzhersh <chregi.glatthard@gmail.com>
2015-09-29 15:20:49 +0200

    make paths independent

--- swissrugbystats/settings.py
+++ swissrugbystats/settings.py
@@ -100,13 +100,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/chregi/Documents/git/swissrugbystats/swissrugbystats/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Media files (Files uploaded by user)
 
-MEDIA_ROOT = '/home/chregi/Documents/git/swissrugbystats/swissrugbystats/media'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # CORS (Cross Origin Resource Sharing)
 # documentation: https://github.com/ottoyiu/django-cors-headers/
