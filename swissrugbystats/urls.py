76ce350e6b66a5f7f829c6eace49d1d2ed49c80d
platzhersh <chregi.glatthard@gmail.com>
2015-09-29 15:03:25 +0200

    try fixing media access

--- swissrugbystats/urls.py
+++ swissrugbystats/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^', include('swissrugbystats.api.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True })
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
