b20a3dcb2038e71165cd5f8a6775ae3b6678d330
platzhersh <chregi.glatthard@gmail.com>
2016-01-13 01:31:45 +0100

    Update tests.py

--- swissrugbystats/crawler/tests.py
+++ swissrugbystats/crawler/tests.py
@@ -2,16 +2,14 @@ from django.core.management import call_command
 from django.test import TestCase
 from django.utils.six import StringIO
 
-# deprecated..
-#class CrawlerTestCase(TestCase):
+class CrawlerTestCase(TestCase):
     """
-    Todo: document.
+    Todo: fix & document.
     Test if the command is callable and returns the right result.
     https://docs.djangoproject.com/en/1.8/topics/testing/tools/#topics-testing-management-commands
     """
-    """
     def test_command_output(self):
-        out = StringIO()
-        call_command('crawl_and_update.py', stdout=out)
-        self.assertIn('blubb', out.getvalue())
-    """
+        #out = StringIO()
+        #call_command('crawl_and_update.py', stdout=out)
+        #self.assertIn('blubb', out.getvalue())
+        pass
