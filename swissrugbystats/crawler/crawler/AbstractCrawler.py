dcf48928ec09241cad08094ac8ee877073db62ad
platzh1rsch <chregi.glatthard@gmail.com>
2019-04-03 21:15:46 +0200

    raise exception for 404 / 500 requests to FSR

--- swissrugbystats/crawler/crawler/AbstractCrawler.py
+++ swissrugbystats/crawler/crawler/AbstractCrawler.py
@@ -15,6 +15,8 @@ class AbstractCrawler(object):
     @classmethod
     def get_soup(cls, url: str) -> Any:
         r = requests.get(url[1], headers=cls.get_request_headers())
+        # raise exception in case of 404 or 500
+        r.raise_for_status()
         return BeautifulSoup(r.text, "html.parser")
 
     @classmethod
